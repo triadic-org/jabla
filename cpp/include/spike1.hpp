e762ae1233f846139042a06833d7d247ced67d34
Elpizo Choi <203277+fuJiin@users.noreply.github.com>
2026-06-03 17:49:37 -0700

    Static -> inline declarations

--- cpp/include/spike1.hpp
+++ cpp/include/spike1.hpp
@@ -2,20 +2,19 @@
 #include <vector>
 
 // Spike buffers. Designed to be #included from one place as one translation unit.
-// File-static state is a single instance - .at() during bring-up.
-static std::vector<float> g_a, g_b, g_c;
-static int g_m, g_k, g_n;
+inline std::vector<float> g_a, g_b, g_c;
+inline int g_m, g_k, g_n;
 
-void mm_set_a(int i, double v) { g_a.at(i) = (float)v; } // <- mm_set_a
-void mm_set_b(int i, double v) { g_b.at(i) = (float)v; } // <- mm_set_b
-double mm_get_c(int i) { return (double)g_c.at(i); } // <- mm_get_c (getter!)
-
-void mm_dims(int m, int k, int n) {
+inline void mm_dims(int m, int k, int n) {
   g_m = m; g_k = k; g_n = n;
   g_a.assign(m*k, 0.0f);
 }
 
-void mm_run() {
+inline void mm_set_a(int i, double v) { g_a.at(i) = (float)v; } // <- mm_set_a
+inline void mm_set_b(int i, double v) { g_b.at(i) = (float)v; } // <- mm_set_b
+inline double mm_get_c(int i) { return (double)g_c.at(i); } // <- mm_get_c (getter!)
+
+inline void mm_run() {
   for (int i = 0; i < g_m; ++i) {
     for (int j = 0; j < g_n; ++j) {
       float s = 0.0f;
